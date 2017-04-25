3ff46574c6a01f4b968574c21241181794619f27
lala <lala@edetw.com>
2017-04-25 14:50:35 +0800

    API 加上單元測是

---
+++ WebApplication1Tests/Controllers/ValuesControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using WebApplication1.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace WebApplication1.Controllers.Tests
+{
+    [TestFixture()]
+    public class ValuesControllerTests
+    {
+        [Test()]
+        public void Get_所有資料()
+        {
+            //arrange
+            var except = 2;
+            //act
+            var c = new ValuesController();
+            var actual = c.Get() as IEnumerable<string>;
+            //assert
+            actual.Should().HaveCount(except, because: "有兩筆資料");
+        }
+        [Test()]
+        public void Get_所有資料ByID()
+        {
+            //arrange
+            var id = 1;
+            var except = "value" + id;
+            //act
+            var c = new ValuesController();
+            var actual = c.Get(id);
+            //assert
+            actual.Should().Be(except, because: "value + id");
+
+        }
+    }
+}
+
+
--- WebApplication1Tests2/Properties/AssemblyInfo.cs
+++ WebApplication1Tests2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("WebApplication1Tests1")]
+[assembly: AssemblyTitle("WebApplication1Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApplication1Tests1")]
+[assembly: AssemblyProduct("WebApplication1Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("c8d6d60b-5da2-474c-b283-27b14b494d70")]
+[assembly: Guid("7d280fd7-3317-4f34-a479-46a32ff0f846")]
 
 // 組件的版本資訊由下列四個值所組成: 
 //
