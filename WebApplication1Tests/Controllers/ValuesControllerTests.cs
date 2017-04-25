4fa6acd614b9e6610da3c21ceb9a2716195e6cc6
lala <lala@edetw.com>
2017-04-25 15:42:40 +0800

    新增測試: 傳入大於2018_3_1的目前日期

--- WebApplication1Tests/Controllers/ValuesControllerTests.cs
+++ WebApplication1Tests/Controllers/ValuesControllerTests.cs
@@ -46,11 +46,19 @@ namespace WebApplication1.Controllers.Tests
         [Test()]
         public void Post_測試一個空物件()
         {
-            Mock.Arrange(() => DateTime.Now).Returns(new DateTime(2018,2,1));
             Mock.Arrange(() => c.ModelState.IsValid).Returns(false);
             var actual = c.Post(new ToDoItem());
             actual.Should().BeOfType<BadRequestResult>(because: "測試一個空物件");
             
+        }
+
+        [Test()]
+        public void Post_測試傳入大於2018_3_1的目前日期()
+        {
+            Mock.Arrange(() => DateTime.Now).Returns(new DateTime(2018, 3, 1));
+            var actual = c.Post(new ToDoItem());
+            actual.Should().BeNull();
+
         }
     }
 }
